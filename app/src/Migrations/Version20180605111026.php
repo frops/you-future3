3ec74920186737f18ab2dd428a70e8106c54ebfb
frops <ifrops@gmail.com>
2018-06-12 17:35:02 +0300

    first final

--- app/src/Migrations/Version20180605111026.php
+++ app/src/Migrations/Version20180605111026.php
@@ -15,7 +15,7 @@ final class Version20180605111026 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, date DATE NOT NULL, seo_descr LONGTEXT DEFAULT NULL, seo_keywords LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, date DATE NOT NULL, seo_descr LONGTEXT DEFAULT NULL, seo_keywords LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
